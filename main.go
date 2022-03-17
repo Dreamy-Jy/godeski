12ec237b28a30924828b6e6817680e57b4370b3e
Jordane Thomas <thomasjordane7@gmail.com>
2022-03-16 20:24:54 -0400

    add start and delete database function

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package odeskidb
 
-import "fmt"
+import (
+	"os"
+)
 
-func PrintHelloWorld() {
-	fmt.Println("Hello World")
+type query interface {
+	isQuery() bool
+}
+
+type Get struct {
+	key string
+}
+type Set struct {
+	key   string
+	value string
+}
+type Delete struct {
+	key string
+}
+type Group struct {
+	queries []interface{}
+}
+
+func (q *Get) isQuery() bool {
+	return true
+}
+func (q *Set) isQuery() bool {
+	return true
+}
+func (q *Delete) isQuery() bool {
+	return true
+}
+func (q *Group) isQuery() bool {
+	return true
+}
+
+// CreateDatabaseIfExist creates a database in the directory
+// specifed by the databaseDirectory param, if a database does
+// not exist there
+func CreateDatabaseIfExist(databaseDirectory string) {
+	err := os.WriteFile(
+		databaseDirectory+"odeski.db",
+		[]byte(""),
+		0644,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// DeleteDatabaseIfExist deletes the database at the directory
+// specifed by the databaseDirectory param, if a database does
+// not exist there
+func DeleteDatabaseIfExist(databaseDirectory string) {
+	err := os.Remove(databaseDirectory + "odeski.db")
+
+	if err != nil {
+		panic(err)
+	}
 }
